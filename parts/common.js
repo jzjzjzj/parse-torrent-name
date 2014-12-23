530058f38237d3ec40100df1ddd4f43dc1ed404c
Jānis Z <jzjzjzj.fwd@gmail.com>
2014-12-23 22:39:37 +0200

    Episode regex doesn't need third group to be capturing.

--- parts/common.js
+++ parts/common.js
@@ -8,7 +8,7 @@ var core = require('../core');
  */
 var patterns = {
   season: /([Ss]?([0-9]{1,2}))[Eex]/,
-  episode: /([Eex]([0-9]{2})([^0-9]|$))/,
+  episode: /([Eex]([0-9]{2})(?:[^0-9]|$))/,
   year: /([\[\(]?((?:19[0-9]|20[01])[0-9])[\]\)]?)/,
   resolution: /[0-9]{3,4}p/,
   quality: /(?:PPV\.)?[HP]DTV|(?:HD)?CAM|B[rR]Rip|TS|(?:PPV )?WEB-?DL(?: DVDRip)?|H[dD]Rip|DVDRip|DVDRiP|DVDRIP|CamRip|W[EB]B[rR]ip|[Bb]lu[Rr]ay|DvDScr|hdtv/,
