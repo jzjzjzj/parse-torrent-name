f46499ce91a25a3ce8fcd53c690ef5f5f2ab0eff
Jānis Z <jzjzjzj.fwd@gmail.com>
2015-01-02 22:25:33 +0200

    Pick correct resolution.

--- parts/common.js
+++ parts/common.js
@@ -10,7 +10,7 @@ var patterns = {
   season: /([Ss]?([0-9]{1,2}))[Eex]/,
   episode: /([Eex]([0-9]{2})(?:[^0-9]|$))/,
   year: /([\[\(]?((?:19[0-9]|20[01])[0-9])[\]\)]?)/,
-  resolution: /[0-9]{3,4}p/,
+  resolution: /(([0-9]{3,4}p))[^M]/,
   quality: /(?:PPV\.)?[HP]DTV|(?:HD)?CAM|B[rR]Rip|TS|(?:PPV )?WEB-?DL(?: DVDRip)?|H[dD]Rip|DVDRip|DVDRiP|DVDRIP|CamRip|W[EB]B[rR]ip|[Bb]lu[Rr]ay|DvDScr|hdtv/,
   codec: /xvid|x264|h\.?264/i,
   audio: /MP3|DD5\.?1|Dual[\- ]Audio|LiNE|DTS|AAC(?:\.?2\.0)?|AC3(?:\.5\.1)?/,
